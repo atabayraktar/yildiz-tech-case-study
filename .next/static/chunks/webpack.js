38fb9d70d99d8983c3eb1d8674d566860f7e3846
ata-bayraktar-appcent <ata.bayraktar@appcent.mobi>
2024-01-06 19:08:07 +0300

    mobile ui done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5dc21765bd05f401"; }
+/******/ 		__webpack_require__.h = function() { return "7d357815a42266b4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
